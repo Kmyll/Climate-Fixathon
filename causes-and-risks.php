69e3feb44f8984a6ab5bc04047a65ededc45fdd8
Camille <camille.desoubrie@gmail.com>
2019-07-28 18:35:22 +0200

    [ADD] causes and risks pages

--- causes-and-risks.php
+++ causes-and-risks.php
@@ -4,10 +4,49 @@
 
 <h1>Causes and risks of climate breakdown</h1>
 
+<h2>How it began</h2>
+
+<p>The origins of "climate change" is linked to the second industrial revolution, starting at around 1880. AT the same time, scientists discover that CO2 is linked to global warming: the world "greenhouse effect" was born. During this second revolution, World power countries like Europle, the USA and Japan developped industries based on oil(petroleum), chemistry, mecanic and electricity. This revolution has been responsible of innovations that are in our everyday lives, such as combustion engine, communication appliances, transports. This revolution also marks the beginning of globalization, with monetary, goods and migrations.</p>
+
+
+<div class="quote">
+  <p>Instead of “climate change” the preferred terms are “climate emergency, crisis or breakdown” and “global heating” is favored over “global warming,” although the original terms are not banned…</p>
+  <p>Katarine Viner - Guardian News & Media</p>
+</div>
 
 
 
+<h2>Main causes of climate breakdown</h2>
+
+<p>Our planet is a living organism so it naturally variates in temperatures and other natural occurences. The causes are therefore multiples and somewhat natural, including volcanism activity, orbital variations and so on. However, human contribution and activities has largly increased its downfall, by:</p>
+<ul>
+  <li>A large increase of the CO2 level (fossil fuel, aerosols, cement manufactures). CO2 is a greenhouse gas which has for consequences to increase the global temperature. According to Le Quéré, C. et al. (2013). The global carbon budget 1959-2011, 87% of CO2 emission are from fossil combustion, including charcoal, gas and oil. The remaining are from deforestation (9%) and industrial process such as cement manufacturing.  Fossil fuel are use for electricity, heating (41% of total fossil emissions), transports, and industries such as electricity power plan, cars, planes and factories </li>
+  <li>Land use</li>
+  <li>Ozone depletion</li>
+  <li>Animal husbandry (cattle, termites and methane)</li>
+  <li>Deforestation</li>
+</ul>
+
+
+
+
+
+<h2>Effects</h2>
+
+<p>According to the Nasa :</p>
+
+<ul>
+  <li>the carbon dyoxide increased by 412 parts per milion (the highest ever recorded)</li>
+  <li>The global temperature increased by 1.9 degrees F since 1880 (and the warmest years are occuring since 2001)</li>
+  <li>The artic ice is decreasing by 12.8% per decades (with its minimum since 2012)</li>
+  <li>Ice sheets are decreasing by 413 gigatonnes per year</li>
+  <li>Sea levels are yearly increasing by 3.3mm (17cm over the past century)</li>
+  <li></li>
+</ul>
+
+<h2>Now</h2>
+
+Now that we know all the causes, we must take action to reverse climate change. And we have 12 years to do so.
+Start now.
 
 </section>
-
-<?php include "footer.php" ?>
---
+++ references.php
@@ -0,0 +1,18 @@
+<?php include "header.php" ?>
+
+<section class="container">
+
+<h1>References</h1>
+
+<h2>Causes of climate breakdown</h2>
+<ul>
+  <li>https://e-rse.net/
+  </li>
+  <li>https://fr.vikidia.org/wiki/Seconde_R%C3%A9volution_industrielle</li>
+  <li>https://climate.nasa.gov/</li>
+  <li>https://en.wikipedia.org/wiki/Climate_change#Climate_variability</li>
+</ul>
+</section>
+
+
+<?php include "footer.php" ?>
